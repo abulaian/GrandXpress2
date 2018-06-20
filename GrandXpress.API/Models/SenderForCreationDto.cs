fd04b99f68fff6337d644e3aea7916445b87491d
Mohamed Ibrahim <moham@LAPTOP-A0RTLI71>
2018-06-20 08:44:08 -0500

    cleaning item and getting more dto's

---
+++ GrandXpress.API/Models/SenderForCreationDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrandXpress.API.Models
+{
+    public class SenderForCreationDto
+    {
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+        [Required]
+        public string Phone { get; set; }
+        public String Address1 { get; set; }
+        public String Address2 { get; set; }
+        public String City { get; set; }
+        public string Country { get; set; }
+    }
+}
